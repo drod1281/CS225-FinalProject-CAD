c4ecb1ce07fec7352457ff0f7352ab37c0bec284
drod1281 <drod128@icloud.com>
2022-11-15 23:28:06 +0000

    we can make now

--- code/src/BTCGraph.cpp
+++ code/src/BTCGraph.cpp
@@ -4,7 +4,38 @@
 #include <list>
 #include <queue>
 
-BTCGraph::BTCGraph(std::string filename) {}
+BTCGraph::BTCGraph(std::string filename) {
+    std::ifstream ifs (filename, std::ifstream::in);
+    std::vector<std::vector<std::string>> content;
+    std::vector<std::string> row;
+    std::string line, word;
+    
+    if(ifs.is_open()) {
+        while(getline(ifs, line)) {
+            row.clear();
+            
+            stringstream str(line);
+            
+            while(getline(str, word, ',')) {
+                row.push_back(word);
+                content.push_back(row);
+            }
+        }
+    }
+
+    for(int i = 0; i < (int)content.size(); i++) {
+        std::vector<std::string> iter = content[i];
+        std::unordered_map<std::string, std::vector<std::pair<std::string, int>>>::iterator it = graph_.find(iter[1]);
+        int rating = stoi(iter[2]);
+        if (it != graph_.end()) {
+            it->second.push_back(std::make_pair(iter[0], rating));
+        } else {
+            std::vector<std::pair<std::string, int>> vect;
+            vect.push_back(std::make_pair(iter[0], rating));
+            graph_[iter[1]] = vect;
+        }
+    }
+}
 
 BTCGraph::~BTCGraph(){}
 
