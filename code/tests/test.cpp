df50d600ea12b068496a4852cd468a7b7378d1c1
Alekx Romo <alekxromo@gmail.com>
2022-11-16 23:53:49 +0000

    tests

--- code/tests/test.cpp
+++ code/tests/test.cpp
@@ -8,7 +8,16 @@
 #include "DrawGraph.h"
 
 TEST_CASE("Correct something idk", "[weight=5]") {
+    V2D roster = file_to_V2D("../tests/data/c5_s10_3_roster.csv"); //change to find the right vector of ratee
 
-    REQUIRE();
+    //change to be the vectors we wrote down
+    const V2D correct_roster = {{"CS577", "JydY", "MnWd", "tnkL"},\
+    {"CS591", "gpDS", "94Ol", "tnkL"}, \
+    {"CS386", "SjC0", "EYge", "Nvu1"}, \
+    {"CS500", "MnWd", "uAcT", "EYge"}, \
+    {"CS395", "gpDS", "EYge", "MnWd"}
+    }; 
+
+    REQUIRE(roster == correct_roster);
 
 }
