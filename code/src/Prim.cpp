c88ed767d2d04700136ffbcae0e345a7ac89fbb4
Alekx Romo <alekxromo@gmail.com>
2022-11-30 22:22:55 +0000

    prim

--- code/src/Prim.cpp
+++ code/src/Prim.cpp
@@ -4,6 +4,10 @@
 #include <list>
 #include <queue>
 
+using namespace std;
+
 PrimMST(){}
 
-PrimMST(unordered_map<string, double> & averages_, string & startPoint){}
+PrimMST(unordered_map<string, double> & averages_, string & startPoint){
+    for(auto string v: averages_){}
+}
